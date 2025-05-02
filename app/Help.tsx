4282099fe92f99190cd6a7c3cb64054e566f1922
Davidmb <davidblanco1993@gmail.com>
2025-05-02 17:59:01 +0200

    fix: scrollview added to help modal

--- app/Help.tsx
+++ app/Help.tsx
@@ -1,13 +1,14 @@
 import AccordionList from '@/components/AccordionList'
 import { generalStyles } from '@/constants/Styles'
 import React from 'react'
-import { Text, View } from 'react-native'
+import { ScrollView } from 'react-native'
+
 
 const help = () => {
   return (
-    <View style={generalStyles.container}>
+    <ScrollView style={generalStyles.container}>
       <AccordionList />
-    </View>
+    </ScrollView>
   )
 }
 
