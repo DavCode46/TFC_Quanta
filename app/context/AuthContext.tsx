e447b27a956c75fe2467e858606c9e3c9e868fdd
Davidmb <davidblanco1993@gmail.com>
2025-05-02 17:57:10 +0200

    fix: redirection on logout

--- app/context/AuthContext.tsx
+++ app/context/AuthContext.tsx
@@ -60,7 +60,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
     setAccountContext(null);
     await AsyncStorage.removeItem('account');
     await AsyncStorage.removeItem('user');
-    router.replace('/Login');
+    router.replace('/');
   };
 
   const triggerReload = () => {
